da3e3a7ba397e444a260013be3eb900cf92d7b61
Nuo Chen <chenn16@cardiff.ac.uk>
2024-07-03 07:38:49 +0800

    [+] init Login page

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,10 +1,43 @@
 <template>
-<div class="login">
-    <div class="box">Login</div>
-</div>
-</template>
+    <div class="login">
+      <div class="box">
+        <h2>Welsh Sport Volunteer Hub</h2>
+        <el-form
+          size="small"
+          ref="formRef"
+          style="max-width: 600px"
+          :model="formData"
+          status-icon
+          :rules="rules"
+          label-width="40px"
+        >
+          <el-form-item label="账号" prop="username">
+            <el-input v-model="formData.username" />
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="formData.password" type="password" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm(formRef)">
+              Login
+            </el-button>
+            <el-button @click="resetForm(formRef)">Reset</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </template>
 <script setup lang='ts'>
+import { onMounted, reactive, ref } from "vue";
+import type { FormInstance, FormRules } from "element-plus";
+// form instance
+const formRef = ref<FormInstance>();
 
+// form data
+const formData = reactive({
+  username: "",
+  password: "",
+});
 </script>
 <style lang='scss'>
 .login{
