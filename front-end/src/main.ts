c609f7b3e7515fee9c84370d3f98f9d67ef3ee66
Nuo Chen <chenn16@cardiff.ac.uk>
2024-07-03 07:01:15 +0800

    [+] import Element-plus

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.ts'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(router).mount('#app')
