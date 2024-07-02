0a7bd31559a9dc4b4d42f21c0839fb289d44d0c3
Nuo Chen <chenn16@cardiff.ac.uk>
2024-07-03 07:02:40 +0800

    [+] edit Login page

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,9 +1,24 @@
 <template>
-<div>login</div>
+<div class="login">
+    <div class="box">Login</div>
+</div>
 </template>
 <script setup lang='ts'>
 
 </script>
 <style lang='scss'>
-
+.login{
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(to bottom, #2866ad, #6894c7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .box{
+        width: 400px;
+        height: 200px;
+        background: #fff;
+        border-radius: 10px;
+    }
+}
 </style>
