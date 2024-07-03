110b48d1a09a0ec7e2a204f784e615d630ccdc30
Nuo Chen <chenn16@cardiff.ac.uk>
2024-07-03 23:04:24 +0800

    [+] add login ElNotification and store token in sessionStorage

--- front-end/src/api/admin.ts
+++ front-end/src/api/admin.ts
@@ -1,8 +1,28 @@
 import { $post } from "../utils/request.ts"
 import { md5 } from "md5js"
+import { ElNotification } from "element-plus";
 
 export const $login = async (params: object | any) => {
     params.password = md5((md5(params.password,32).split('').reverse().join('')),32);
     let res = await $post("login", params);
     console.log(res);
+    const { code, data } = res;
+  // console.log(data[0].message);
+  if (code === 200) {
+    ElNotification({
+      title: "Notification",
+      message: data.message,
+      type: "success",
+    });
+    // if login success, store token in sessionStorage
+    sessionStorage.setItem("token", data.token);
+    return res;
+  } else {
+    ElNotification({
+      title: "Notification",
+      message: data.message,
+      type: "error",
+    });
+  }
+  return res;
 }
