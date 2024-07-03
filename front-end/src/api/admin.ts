c8459eaec0edd4eaedd4c1b2301fb9e8806a38e0
Nuo Chen <chenn16@cardiff.ac.uk>
2024-07-03 23:00:24 +0800

    [+] add password hashing with MD5

--- front-end/src/api/admin.ts
+++ front-end/src/api/admin.ts
@@ -1,6 +1,8 @@
 import { $post } from "../utils/request.ts"
+import { md5 } from "md5js"
 
-export const $login = async (params: object) => {
+export const $login = async (params: object | any) => {
+    params.password = md5((md5(params.password,32).split('').reverse().join('')),32);
     let res = await $post("login", params);
     console.log(res);
 }
