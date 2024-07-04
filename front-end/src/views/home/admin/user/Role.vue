ba0e623e64178e509b57ede1e010617a8f377eff
Nuo Chen <chenn16@cardiff.ac.uk>
2024-07-04 22:00:25 +0800

    [#] edit Role page

--- front-end/src/views/home/admin/user/Role.vue
+++ front-end/src/views/home/admin/user/Role.vue
@@ -1,7 +1,47 @@
 <template>
-  <div class="role">role</div>
+  <div class="role">
+    <el-table :data="roles" stripe style="width: 100%">
+      <el-table-column prop="roleId" label="Role ID" width="100" />
+      <el-table-column prop="roleName" label="RoleName" width="250" />
+      <el-table-column label="edit">
+        <template #default="scope">
+          <el-button size="small" @click="handleEdit(scope.row.roleId)">
+            Edit
+          </el-button>
+          <el-button
+            size="small"
+            type="danger"
+            @click="handleDelete(scope.row.roleId)"
+          >
+            Delete
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { $list } from "../../api/role.ts";
+
+// role list
+let roles = ref([]);
+// load role list
+const loadRoles = async () => {
+  roles.value = await $list();
+};
+// edit role
+const handleEdit = (roleId: number) => {
+  console.log(roleId);
+};
+// delete role
+const handleDelete = (roleId: number) => {
+  console.log(roleId);
+};
+onMounted(() => {
+  loadRoles();
+});
+</script>
 
 <style lang="scss"></style>
