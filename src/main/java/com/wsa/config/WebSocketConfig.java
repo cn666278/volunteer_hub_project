c55a962583a7a15b626f7983ca0b8fd44005f8bb
Nuo Chen <chenn16@cardiff.ac.uk>
2024-08-21 18:16:02 +0100

    [*] update WebSocketConfig

--- src/main/java/com/wsa/config/WebSocketConfig.java
+++ src/main/java/com/wsa/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("http://10.72.102.12:5173")
                 .withSockJS();
     }
 }
