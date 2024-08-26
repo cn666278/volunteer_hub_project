cdb76d838f0242e01d040b45636c247d141e62be
Nuo Chen <chenn16@cardiff.ac.uk>
2024-08-27 04:10:33 +0800

    [+] add theme button

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -12,18 +12,32 @@ import './api/mock.ts'
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import 'vue-cal/dist/vuecal.css';
 import { initI18n } from './locales/index.ts'
+import useThemeStore from './store/theme'; // 引入主题store
+
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 window.global = global;
+
 const app = createApp(App);
+
+// 注册Element Plus图标组件
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);  
 }
+
+// 初始化多语言支持
 initI18n(app)
+
+// 设置全局属性
 app.config.globalProperties.$api = api;
+
+// 初始化主题
+const themeStore = useThemeStore(pinia); // 传递 pinia 实例
+themeStore.initializeTheme(); // 确保页面初次加载时应用主题
+
+// 挂载Vue应用
 app.use(ElementPlus).use(router).use(pinia).mount('#app')
 
 // if (process.env.NODE_ENV === 'production') {
-    // console.log = function () {};
-//   }
-  
+//     console.log = function () {};
+// }
