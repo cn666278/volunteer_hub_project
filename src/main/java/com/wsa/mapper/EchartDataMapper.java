c3bf0ebddbbcabc43ed0db97e259e249381fe31a
Nuo Chen <chenn16@cardiff.ac.uk>
2024-08-27 08:28:44 +0800

    [#] update for data

--- src/main/java/com/wsa/mapper/EchartDataMapper.java
+++ src/main/java/com/wsa/mapper/EchartDataMapper.java
@@ -51,14 +51,14 @@ public interface EchartDataMapper {
     // 获取 userData 中的数据
     @Select("SELECT " +
             "DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL seq DAY), '%a') AS date, " +
-            "COALESCE(SUM(CASE WHEN DATE(registerTime) = DATE(DATE_SUB(CURDATE(), INTERVAL seq DAY)) THEN 1 ELSE 0 END), 0) AS newUsers, " +
-            "COALESCE(SUM(CASE WHEN DATE(lastLoginTime) = DATE(DATE_SUB(CURDATE(), INTERVAL seq DAY)) THEN 1 ELSE 0 END), 0) AS activeUsers " +
-            "FROM (SELECT @row := @row + 1 AS seq FROM (" +
-            "SELECT 0 UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6) t1, " +
-            "(SELECT @row := -1) t2) seqs " +
-            "LEFT JOIN users u ON DATE(u.registerTime) = DATE(DATE_SUB(CURDATE(), INTERVAL seq DAY)) " +
-            "OR DATE(u.lastLoginTime) = DATE(DATE_SUB(CURDATE(), INTERVAL seq DAY)) " +
-            "GROUP BY seq " +
+            "SUM(CASE WHEN DATE(u.registerTime) = DATE(DATE_SUB(CURDATE(), INTERVAL seq DAY)) THEN 1 ELSE 0 END) AS newUsers, " +
+            "SUM(CASE WHEN DATE(u.lastLoginTime) = DATE(DATE_SUB(CURDATE(), INTERVAL seq DAY)) THEN 1 ELSE 0 END) AS activeUsers " +
+            "FROM (SELECT 0 AS seq UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6) seqs " +
+            "LEFT JOIN users u ON (DATE(u.registerTime) = DATE(DATE_SUB(CURDATE(), INTERVAL seqs.seq DAY)) " +
+            "OR DATE(u.lastLoginTime) = DATE(DATE_SUB(CURDATE(), INTERVAL seqs.seq DAY))) " +
+            "WHERE DATE(DATE_SUB(CURDATE(), INTERVAL seqs.seq DAY)) BETWEEN DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) " +
+            "AND DATE_ADD(DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY), INTERVAL 6 DAY) " +
+            "GROUP BY seqs.seq " +
             "ORDER BY FIELD(DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL seq DAY), '%a'), 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')")
     List<EchartDataRes.UserData> getUserData();
 }
